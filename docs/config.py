309b1945168fc7d62d023167f0211456ba8e8ebc
Jagath Jai Kumar <jagath.jaikumar@gmail.com>
2021-02-07 00:21:03 -0500

    add doc folder

---
+++ docs/config.py
@@ -0,0 +1,12 @@
+project = "RescueLab Labeler"
+copyright = "2021, Jagath Jai Kumar"
+author = "Jagath Jai Kumar"
+
+extensions = ["autoapi.extension"]
+templates_path = ["templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+html_theme = "classic"
+
+autoapi_dirs = ["../labeler_api"]
+autoapi_keep_files = False
---
+++ docs/index.rst
@@ -0,0 +1,17 @@
+.. RescueLab Labeler documentation master file
+
+Welcome to RescueLab Labeler's documentation!
+==================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
