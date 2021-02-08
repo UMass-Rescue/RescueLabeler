6a6e57ed2d6f8c05b398b7068a464998abd4bf9b
Jagath Jai Kumar <jagath.jaikumar@gmail.com>
2021-02-07 19:56:32 -0500

    adding routers

--- labeler_api/app/api/__init__.py
+++ labeler_api/app/api/__init__.py
@@ -1,6 +1,5 @@
-from fastapi import APIRouter, Depends
-from fastapi.security import OAuth2PasswordBearer
-from app.db import DBContextManager
+from fastapi import APIRouter
+from app.api import administration, configuration, labeling
 import logging
 
 logger = logging.getLogger("rescue-labeler")
@@ -8,26 +7,16 @@ logger = logging.getLogger("rescue-labeler")
 
 router = APIRouter()  # Primary router
 
-# TODO: Remove
-# router.include_router(authentication.router, tags=["authentication"], prefix="/users")
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+router.include_router(
+    administration.router, tags=["administration"], prefix="/administration"
+)
+router.include_router(
+    configuration.router, tags=["configuration"], prefix="/configuration"
+)
+router.include_router(labeling.router, tags=["labeling"], prefix="/labeling")
 
 
 @router.get("/")
 async def home():
     """Default home route"""
     return {"msg": "RescueLab Labeler"}
-
-
-@router.get("/items/")
-async def read_items(token: str = Depends(oauth2_scheme)):
-    """TODO update"""
-    return {"token": token}
-
-
-@router.get("/db_test/")
-async def db_test():
-    with DBContextManager() as db:
-        logger.info(db)
-    return {"hi": "hey"}
